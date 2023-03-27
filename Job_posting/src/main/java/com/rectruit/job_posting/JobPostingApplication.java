038577986ae501c52328fe2b9319cf92adcc929a
NMagpie <sorochin.work@gmail.com>
2023-03-27 17:52:27 +0300

    Initial Commit Of The Project

---
+++ Job_posting/src/main/java/com/rectruit/job_posting/JobPostingApplication.java
@@ -0,0 +1,13 @@
+package com.rectruit.job_posting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPostingApplication.class, args);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Recruitment_System
 Diploma Thesis Work of creation of Recruitment System
+
+Made by:
+* Birsan Andreea, FAF-193
+* Sorochin Nichita, FAF-191
+
+Supervisor:
+Gavrilita Mihail, asist. univ.
